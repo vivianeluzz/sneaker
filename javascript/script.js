6872c6d09dde5c02ff949877b67e77fc40bdc4e4
Viviane <vivivianesluzz@gmail.com>
2024-06-16 18:20:18 -0300

    Inserido mensagem hover no banner

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,14 @@
 const copy = document.querySelector('.carousel-logos').cloneNode(true);
 document.querySelector('.slider-logos').appendChild(copy);
+
+
+const contentHover = document.getElementById("content-hover");
+
+
+function showMessage() {
+    contentHover.style.display = "flex";
+}
+
+function hideMessage() {
+    contentHover.style.display = "none";
+}
